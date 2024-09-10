1a28bae78fdac20123e6023d54fcefbc3eae07d5
Muhammad Shahid Yusuf <117685623+shahidyusuf@users.noreply.github.com>
2024-09-10 21:40:46 +0500

    final software complete

--- Software(LMS_Software_Submission)/src/config/firebase.jsx
+++ Software(LMS_Software_Submission)/src/config/firebase.jsx
@@ -1,22 +1,20 @@
-// src/config/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBSuOoCcT3lMUgesCPv7toTjtWlQkM6NtA",
-  authDomain: "signup-form-b46b8.firebaseapp.com",
-  projectId: "signup-form-b46b8",
-  storageBucket: "signup-form-b46b8.appspot.com",
-  messagingSenderId: "428252044078",
-  appId: "1:428252044078:web:7a17c0fea3c06339cbb8bf"
+  apiKey: "AIzaSyD1uax-0-qL-fu49RU8e2qrrEKz-OPnxcc",
+  authDomain: "software-lms-41785.firebaseapp.com",
+  projectId: "software-lms-41785",
+  storageBucket: "software-lms-41785.appspot.com",
+  messagingSenderId: "1021129885925",
+  appId: "1:1021129885925:web:d2662d34f73c4efe822c0c"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and Firestore
 const auth = getAuth(app);
 const database = getFirestore(app);
 
